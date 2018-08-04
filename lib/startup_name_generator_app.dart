156639ba29790e5a292956fda4eb5b7d9fb05d79
kentrino <tachikoma@h-kento.jp>
2018-08-04 18:16:14 +0900

    :sparkles: move app code to another file

---
+++ lib/startup_name_generator_app.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class MyStartupNameGeneratorApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return new MaterialApp(
+        title: 'Startup Name Generator',
+        theme: new ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+          // counter didn't reset back to zero; the application is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: RandomWords()
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(WordPair pair) {
+    final bool isAlreadySaved = _saved.contains(pair);
+    return ListTile(
+        title: Text(
+            pair.asPascalCase,
+            style: _biggerFont
+        ),
+        trailing: new Icon(
+          isAlreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: isAlreadySaved ? Colors.red: null,
+        ),
+        onTap: () {
+          setState(() {
+            if (isAlreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        }
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(itemBuilder: (context, i) {
+      // TODO: what is Divider. 区切り？
+      if (i.isOdd) return Divider();
+      // divide and Math.floor
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  void _pushSaved() {
+    // contextというのがStateにはいるっぽい
+    Navigator.of(context).push(
+        new MaterialPageRoute<void>(builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                  (WordPair pair) {
+                return new ListTile(
+                    title: new Text(
+                        pair.asPascalCase,
+                        style: _biggerFont
+                    )
+                );
+              }
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+              context: context,
+              tiles: tiles)
+              .toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text("Saved Suggestions"),
+            ),
+            body: new ListView(children: divided),
+          );
+        })
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Startup Name Generator ☆"),
+          // TODO: <Widget>[] の表記は配列のコンストラクタ？
+          actions: <Widget>[
+            // TODO: constは何？
+            new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
+          ],
+        ),
+        body: _buildSuggestions()
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
